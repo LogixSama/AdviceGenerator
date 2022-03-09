c5943db3ff49aad896ee7a25e05468d8e5d8666b
Shreyan Kashyap <16shreyan@gmail.com>
2022-03-09 21:08:09 +0530

    final changes

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-fetch("https://api.adviceslip.com/advice")
-.then(res =>
-    res.json()
-.then(advice =>{
+var adviceText = document.querySelector(".advice-text");
+var headingText = document.querySelector(".heading-text");
+console.log(adviceText)
 
-    console.log(advice)
+
+function fetchNewAdvice(){
+    fetch("https://api.adviceslip.com/advice")
+        .then(res =>
+            res.json()
+                .then(obj => {
+                    console.log(obj)
+                    let { slip } = obj
+                    console.log(slip.id,slip.advice)
+                    adviceText.innerHTML= `"${slip.advice}"`
+                    headingText.innerHTML= `A D V I C E # ${slip.id}`
+                }
+            )
+    )
 }
-))
